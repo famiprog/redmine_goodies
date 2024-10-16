c4557af7490ec533bc4ca83ca8652cf2b01fc892
Alexandru Luca <lucaalexandru06@yahoo.com>
2024-10-16 16:12:34 +0300

    #35739-moving-cross-linking-in-js

--- assets/javascripts/questions_system.js
+++ assets/javascripts/questions_system.js
@@ -10,3 +10,23 @@ function addAnswerMacroToNotesEditor(note_id, question_number) {
         notesEditor.focus();
     } 
 }
+
+function markAnsweredQuestions() {
+    const notes = document.querySelectorAll(".message-content");
+    notes.forEach(noteContent => {
+        /**
+         * eg: "[Answer for #note-26, 2/] In my opinion, this is the solution."
+         * the regex below will match: "[Answer for #note-26, 2/]"
+         * and it will extract: "26" & "2"
+         */
+        const answers = noteContent.innerHTML.matchAll(/\[Answer for <a href="#note-\d+">#note-(\d+)<\/a>, (\d+)\/\]/g);
+        answers.forEach(answer => {
+            const questionNoteId = answer[1];
+            const questionNumber = answer[2];
+            const answerNoteId = noteContent.parentElement.parentElement.id;
+            const answeredTextId = `answered-text-${questionNoteId}-${questionNumber}`;
+            const answeredTextElement = document.getElementById(answeredTextId);
+            answeredTextElement.innerHTML = `<i class="icon icon-checked" style="padding-left: 15px;"></i><span>Answered in <a href=#${answerNoteId}>#${answerNoteId}</a></span>,&nbsp;`;
+        });
+    });
+}
