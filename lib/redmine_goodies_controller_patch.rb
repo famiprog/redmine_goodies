c554a063a2febb9bb40d804f349ba888330c3571
Alexandru Luca <lucaalexandru06@yahoo.com>
2024-10-17 11:08:24 +0300

    #33666-improving-comm-regex

--- lib/redmine_goodies_controller_patch.rb
+++ lib/redmine_goodies_controller_patch.rb
@@ -17,16 +17,28 @@ module RedmineGoodiesControllerPatch
                 # e.g. 
                 # <div id="note-35" class="note">
                 #     ...
-                #     <div journal-118-notes class="wiki">
+                #     <div id="journal-118-notes" class="wiki">
                 #          <p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p></div>
                 #          <p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p>
                 #     </div>
                 #     ...
                 # </div>
+                # ...
+                # <div id="note-36" class="note">
+                #     ...
+                #     <div id="journal-119-notes" class=\"wiki\">
+                #          <p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p></div>
+                #     </div>
+                #     ...
+                # </div>
+                #
                 # The regex below will match:
                 # <div journal-118-notes class="wiki">
                 #      <p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p></div>
                 #      <p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p>
+                # </div>
+                # <div id="journal-119-notes" class=\"wiki\">
+                #      <p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p></div>
                 # </div>
                 response.body = response.body.gsub(/<div[^>]*class=(\\?"?)wiki(\\?"?)[^>]*>(.*?)<\/div>/m) do |div_class_wiki|
                     # e.g. <div class="..."><p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p></div>, this regex will match: 
