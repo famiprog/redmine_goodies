779e789b00c7749695e3a707a1df8a7bfbe0398c
Alexandru Luca <lucaalexandru06@yahoo.com>
2024-10-30 15:03:02 +0200

    #33666-small-regex-update

--- lib/redmine_goodies_controller_patch.rb
+++ lib/redmine_goodies_controller_patch.rb
@@ -17,11 +17,12 @@ module RedmineGoodiesControllerPatch
                 # e.g. 
                 # <div class="...">
                 #   <p><img src="/attachments/thumbnail/91/clipboard-202408191234-gdasdu.png" alt=""></p>
+                #   <img alt="clipboard-202410301333-uwhg6.png" class="filecontent image" src="/attachments/download/253/clipboard-202410301333-uwhg6.png">
                 #   <p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p>
                 #   <p><img srcset="/attachments/thumbnail/98/200 2x" style="max-width: 100px; max-height: 100px;" src="/attachments/thumbnail/98/200"></p>
                 # </div>, this regex will match: 
                 # "<img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt="">"
-                response.body = response.body.gsub(/<img(?![^>]*\bsrcset\b)[^>]*\bsrc="\/attachments\/download[^"]*"[^>]*>/) do |img|
+                response.body = response.body.gsub(/<img(?![^>]*\b(class="[^"]*\bfilecontent\b[^"]*"|\bsrcset\b))[^>]*\bsrc="\/attachments\/download[^"]*"[^>]*>/) do |img|
                     "<details class=\"collapsible-img\"><summary><span class=\"icon-image-collapsible\"></span><a class=\"expand-collapse-btn\" onclick=\"addBackgroundToCollapsibleImage(event.target?.parentElement.parentElement)\">Click to expand/collapse image</a> | <a href=\"#\" onclick=\"expandAll(event); return false;\">Expand all</a> | <a href=\"#\" onclick=\"collapseAll(event); return false;\">Collapse all</a></summary>#{img}</details>"
                 end
             end
