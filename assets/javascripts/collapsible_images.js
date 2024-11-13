cb33defe7421a6fc984f9a104478e40153667256
Alex Luca <lucaalexandru06@yahoo.com>
2024-11-13 11:09:37 +0200

    #33666-add-background-to-collapsible-img-and-regex-adjustments (#6)
    * #33666-add-background-to-collapsible-img-and-regex-adjustments
    
    * #33666-small-regex-update

--- assets/javascripts/collapsible_images.js
+++ assets/javascripts/collapsible_images.js
@@ -1,26 +1,50 @@
-function expandAll(containerElement) {
+function expandAll(event) {
     localStorage.setItem("isCollapsed", "false");
-    const element = document.querySelector(containerElement);
+    const element = document.querySelector("#main");
     const detailsElements = element.querySelectorAll(".collapsible-img");
     detailsElements.forEach(function(detailsElement) {
         if (!detailsElement.hasAttribute("open")) {
             detailsElement.setAttribute("open", true);
         }
     });
+    addBackgroundToCollapsibleImage(event.target?.parentElement.parentElement);
 }
 
-function collapseAll(containerElement) {
+function collapseAll(event) {
     localStorage.setItem("isCollapsed", "true");
-    const element = document.querySelector(containerElement);
+    const element = document.querySelector("#main");
     const detailsElements = element.querySelectorAll(".collapsible-img");
     detailsElements.forEach(function(detailsElement) {
         if (detailsElement.hasAttribute("open")) {
             detailsElement.removeAttribute("open");
         }
     });
+    addBackgroundToCollapsibleImage(event.target?.parentElement.parentElement);
 }
 
 function getIsCollapsedValueFromLocalStorage() {
     const isCollapsed = localStorage.getItem("isCollapsed");
     isCollapsed === "true" ? collapseAll("#main") : expandAll("#main");
+}
+
+function addBackgroundToCollapsibleImage(detailsElement) {
+    if (!detailsElement) { return; }
+    detailsElement.classList.add("collapsible-highlighted");
+    if (!isElementInViewport(detailsElement)) {
+        setTimeout(() => {
+            detailsElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 500);
+    }
+    setTimeout(() => {
+        detailsElement.classList.remove("collapsible-highlighted");
+    }, 2000);
+}
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 && rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
--- lib/redmine_goodies_controller_patch.rb
+++ lib/redmine_goodies_controller_patch.rb
@@ -16,12 +16,14 @@ module RedmineGoodiesControllerPatch
             if RedmineGoodiesSettings.collapsible_images?
                 # e.g. 
                 # <div class="...">
+                #   <p><img src="/attachments/thumbnail/91/clipboard-202408191234-gdasdu.png" alt=""></p>
+                #   <img alt="clipboard-202410301333-uwhg6.png" class="filecontent image" src="/attachments/download/253/clipboard-202410301333-uwhg6.png">
                 #   <p><img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt=""></p>
                 #   <p><img srcset="/attachments/thumbnail/98/200 2x" style="max-width: 100px; max-height: 100px;" src="/attachments/thumbnail/98/200"></p>
                 # </div>, this regex will match: 
                 # "<img src="/attachments/download/90/clipboard-202408191804-gdzhu.png" alt="">"
-                response.body = response.body.gsub(/<img(?![^>]*\bsrcset\b)[^>]*>/) do |img|
-                    "<details class=\"collapsible-img\"><summary><span class=\"icon-image-collapsible\"></span><a class=\"expand-collapse-btn\">Click to expand/collapse image</a> | <a href=\"#\" onclick=\"expandAll(&quot;#main&quot;); return false;\">Expand all</a> | <a href=\"#\" onclick=\"collapseAll(&quot;#main&quot;); return false;\">Collapse all</a></summary>#{img}</details>"
+                response.body = response.body.gsub(/<img(?![^>]*\b(class="[^"]*\bfilecontent\b[^"]*"|\bsrcset\b))[^>]*\bsrc="\/attachments\/download[^"]*"[^>]*>/) do |img|
+                    "<details class=\"collapsible-img\"><summary><span class=\"icon-image-collapsible\"></span><a class=\"expand-collapse-btn\" onclick=\"addBackgroundToCollapsibleImage(event.target?.parentElement.parentElement)\">Click to expand/collapse image</a> | <a href=\"#\" onclick=\"expandAll(event); return false;\">Expand all</a> | <a href=\"#\" onclick=\"collapseAll(event); return false;\">Collapse all</a></summary>#{img}</details>"
                 end
             end
         end
