c4ebda2bbdbd93e37e6e9838e93d8f19cdf45ff8
Teodor Duta <teodor.duta@resonate-mp4.com>
2024-11-18 15:11:33 +0200

    #32683 - added details/details w/ summary macros

---
+++ assets/javascripts/details_system.js
@@ -0,0 +1,23 @@
+jsToolBar.prototype.elements.space7= {
+    type: 'space',
+};
+
+jsToolBar.prototype.elements.details_macro = {
+    type: 'button',
+    title: 'Details macro',
+    fn: {
+        wiki: function () {
+            this.encloseLineSelection('<details>\n\n', '\n\n</details>');
+        }
+    }
+};
+
+jsToolBar.prototype.elements.details_with_summary_macro = {
+    type: 'button',
+    title: 'Details with summary macro',
+    fn: {
+        wiki: function () {
+            this.encloseLineSelection('<details><summary>Click to expand</summary\n\n', '\n\n</details>');
+        }
+    }
+};
