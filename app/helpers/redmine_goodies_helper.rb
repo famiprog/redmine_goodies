c0d689eab675738c906f4707092d8cfc19c56ec7
Alexandru Luca <lucaalexandru06@yahoo.com>
2025-09-12 16:20:40 +0300

    #36063-adding-remarks

--- app/helpers/redmine_goodies_helper.rb
+++ app/helpers/redmine_goodies_helper.rb
@@ -9,7 +9,7 @@ module RedmineGoodiesHelper
         from_value = field_actions[JSON_FIELDS[:FROM_VALUE]]
         to_value = field_actions[JSON_FIELDS[:TO_VALUE]]
         actions = field_actions[JSON_FIELDS[:ACTIONS]]
-        issue_field_value = get_field_value(field, issue)
+        issue_field_value = get_field_value_by_label(field, issue)
         attribute_previous_value = get_field_previous_value(field, issue)  # if blank, the issue field didn't change
         from_value = nil if from_value == "nil"
         to_value = nil if to_value == "nil"
@@ -19,12 +19,13 @@ module RedmineGoodiesHelper
                 (from_value.blank? && issue_field_value == to_value && !attribute_previous_value.blank? && attribute_previous_value != to_value) ||
                 (issue_field_value == to_value && !attribute_previous_value.blank? && attribute_previous_value == from_value)
             perform_actions(actions, issue, field, issue_field_value)
-            return
+            # return
         end
     end
 
     def perform_actions(actions, issue, field, issue_field_value)
         changes_logs = []
+        issue.custom_field_values if issue.custom_field_values.empty?
         actions.each do |action|
             if action[JSON_FIELDS[:A_ACTION]] == ACTION_TYPES[:RESET_FIELD]
                 was_action_applied = modify_custom_field_value(action[JSON_FIELDS[:A_FIELD]], issue, nil)
@@ -41,43 +42,33 @@ module RedmineGoodiesHelper
     end
 
     def modify_custom_field_value(cf_name, issue, value)
-        cf_issue = issue.custom_field_values.find { |cfv| cfv.custom_field.name == cf_name }
-        
+        column = nil
+        I18n.with_locale(:en) do
+            column = IssueQuery.new.available_columns.find { |col| col.caption == cf_name }
+        end
+
+        cf_issue = issue.custom_field_values.find { |cfv| cfv.custom_field.name == column.custom_field.name }
         if cf_issue.nil?
             flash[:warning] = l(:actions_to_trigger_when_fields_are_changed_status_warning, cf_name: cf_name, issue_id: issue.id)
             return false
         end
 
         if value.nil?
-            cf_definition = CustomField.find_by(name: cf_name)
-            cf_issue.value = cf_definition.default_value.presence || nil
+            cf_issue.value = cf_issue.custom_field.default_value.presence || nil
         else
             cf_issue.value = !value.is_a?(ActiveSupport::TimeWithZone) ? value : value.strftime("%m/%d/%Y")
         end
         return true
     end
 
-    def get_issue_field(label, issue)
-        # issue attributes, such as: Status, Assignee, Subject etc
-        issue_attributes = Issue.new.attributes.keys
-        issue_attributes.each do |attribute|
-          attribute = attribute.sub(/_id$/, '')  # eg: status_id -> status
-          field_label = I18n.t("field_#{attribute}")
-          return issue.send(attribute) if field_label == label
+    def get_field_value_by_label(label, issue)
+        column = nil
+        I18n.with_locale(:en) do
+            column = IssueQuery.new.available_columns.find { |col| col.caption == label }
         end
-        
-        custom_field = IssueCustomField.find_by(name: label)
-        if custom_field
-          custom_value = CustomValue.find_by(custom_field_id: custom_field.id, customized_id: issue.id)
-          return custom_value&.value
-        end
-        return nil
-    end
-
-    def get_field_value(field, issue)
-        issue_field = get_issue_field(field, issue)
-        issue_field_value = issue_field.respond_to?(:name) ? issue_field.name : issue_field
-        return issue_field_value
+        return nil unless column
+        value = column.value(issue)
+        return value.respond_to?(:name) ? value.name : value.to_s
     end
 
     def get_field_previous_value(attr_name, issue)
@@ -98,23 +89,26 @@ module RedmineGoodiesHelper
     end
 
     def get_field_value_by_id(attribute_name, id, issue_id)
-        fields_values_by_id = {
-            I18n.t(:field_status) => ->(id) { IssueStatus.find_by(id: id)&.name },
-            I18n.t(:field_assigned_to) => ->(id) { User.find_by(id: id)&.name },
-            I18n.t(:field_priority) => ->(id) { IssuePriority.find_by(id: id)&.name },
-            I18n.t(:field_category) => ->(id) { IssueCategory.find_by(id: id)&.name },
-            I18n.t(:field_project) => ->(id) { Project.find_by(id: id)&.name },
-            I18n.t(:field_tracker) => ->(id) { Tracker.find_by(id: id)&.name }
-        }
-        get_field_value = fields_values_by_id[attribute_name]
-        value = get_field_value ? get_field_value.call(id) : nil
-        return value if !value.nil?
-
+        column = nil
+        I18n.with_locale(:en) do
+            column = IssueQuery.new.available_columns.find { |col| col.caption == attribute_name }
+        end
+        return nil unless column
+        model_class = case column.name.to_s
+            when 'status'      then IssueStatus
+            when 'assigned_to' then User
+            when 'priority'    then IssuePriority
+            when 'category'    then IssueCategory
+            when 'project'     then Project
+            when 'tracker'     then Tracker
+        end
+        return model_class&.find_by(id: id)&.name if model_class
         custom_field = IssueCustomField.find_by(name: attribute_name)
         if custom_field
             custom_value = CustomValue.find_by(custom_field_id: custom_field.id, customized_id: issue_id)
             return custom_value&.value
         end
+        return nil
     end
 
     def display_fields_changes_status(field, list)
