89db90c204c6a395f38aa20551c2bf7f454f2105
Cristian Spiescu <cristian.spiescu@crispico.com>
2026-02-23 13:24:38 +0100

    Fixed regression: reposition menu didn't work any more

--- lib/redmine_goodies_hook_listener.rb
+++ lib/redmine_goodies_hook_listener.rb
@@ -1,5 +1,6 @@
 class RedmineGoodiesHookListener < Redmine::Hook::ViewListener
+	# Reorder before global_macros so reorder.js loads first; reposition (in global_macros) must run after it.
+	render_on :view_layouts_base_html_head, :partial => "redmine_goodies/reorder"
 	render_on :view_layouts_base_html_head, :partial => "redmine_goodies/global_macros"
 	render_on :view_issues_context_menu_start, :partial => "redmine_goodies/quick_edit_context_menu"
-	render_on :view_layouts_base_html_head, :partial => "redmine_goodies/reorder"
 end
